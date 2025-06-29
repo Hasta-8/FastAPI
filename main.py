0621d4f23a9357bc1c06929d0ff1126bfdc601d3
Debadri Chatterjee <debadrichatterjee1997@gmail.com>
2025-06-29 22:33:37 +0530

    Done till post creation

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, Ranch0i!"}
+    return {"message": "Hello, Ranchi!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts data"}
+
+@app.post("/create-post")
+def create_post():
+    return {"message": "Post created successfully"}
+
+https://youtu.be/0sOvCWFmrtA?si=G7Z6XnL4f5oXTCyd&t=3710
+
+

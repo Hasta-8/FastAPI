8436ca3ba8c806237e6998ae13deae8c258c7f69
Debadri Chatterjee <debadrichatterjee1997@gmail.com>
2025-06-30 21:02:00 +0530

    Intro to pydantic and it's data validation properties

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    Title: str
+    Content: str
+    Published: bool = True
+    Rating: Optional[int] = None
 
 @app.get("/")
 def read_root():
@@ -11,9 +20,11 @@ def get_posts():
     return {"data": "This is your posts data"}
 
 @app.post("/create-post")
-def create_post():
-    return {"message": "Post created successfully"}
+def create_post(post: Post):
+    print(post.Rating)
+    print(post.dict())
+    return {"data": post}
 
-https://youtu.be/0sOvCWFmrtA?si=G7Z6XnL4f5oXTCyd&t=3710
+
 
 
